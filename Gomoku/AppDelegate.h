b2000aa728265083324ac915b0238a3cbed7ae2e
Changchang Wang <Jeffreywang1943@gmail.com>
2017-05-21 15:09:29 +1000

    Initial commit

---
+++ Gomoku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gomoku
+//
+//  Created by Changchang on 21/5/17.
+//  Copyright © 2017 University of Melbourne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gomoku/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Gomoku
+//
+//  Created by Changchang on 21/5/17.
+//  Copyright © 2017 University of Melbourne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
